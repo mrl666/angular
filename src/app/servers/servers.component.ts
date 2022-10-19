e79d61e69f8c77fb5fb690f21dbd26cbb6b402d7
Marcelino Rutea López <marcelino.rutea@civica-soft.com>
2022-10-19 14:30:21 +0200

    component added

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+
+  constructor() {
+    console.log('contructor');
+   }
+
+  ngOnInit(): void {
+    console.log('oninit');
+  }
+
+}
